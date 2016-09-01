923144cfd266d647a451c94542bce6ee6b6878df
kabary <ahmed.alkabary@gmail.com>
2016-08-31 18:09:11 -0600

    Case statements practise

---
+++ sleep-walking.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+case "$1" in 
+
+	start)
+	/tmp/sleep-walking-server &
+	;;
+
+	stop)
+	
+	kill $(cat /tmp/sleep-walking-server.pid)
+	;;	
+
+	*)
+	 echo "Usage sleep-walking start|stop"
+	;;
+esac
+
